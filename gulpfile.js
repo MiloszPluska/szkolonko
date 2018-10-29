0efe77a1d13ea8ea6daf670f7cd0161b19298c24
Milosz Pluska <milosz@january15th.com>
2018-10-29 15:44:01 +0100

    add - browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp'),
     autoprefixer = require('autoprefixer'),
     cssvars = require('postcss-simple-vars'),
     nested = require('postcss-nested');
+    cssImport = require('postcss-import'),
+    browserSync = require('browser-sync').create();
 
 
 
@@ -17,17 +19,31 @@ gulp.task('html', function () {
 
 gulp.task('styles', function () {
    return gulp.src('./app/assets/styles/styles.css')
-       .pipe(postcss([nested, cssvars, autoprefixer]))
+       .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
        .pipe(gulp.dest('./app/temp/styles'));
 });
 
 
 
 gulp.task('watch', function () {
-    watch('./app/index.html', function () {
-        gulp.start('html');
+    notify: false,
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
     });
-    watch('./app/assets/styles/styles.css', function () {
-        gulp.start('styles');
+
+
+
+    watch('./app/index.html', function () {
+       browserSync.reload();
+    });
+    watch('./app/assets/styles/**/*.css', function () {
+        gulp.start('cssInject');
     });
 });
+
+gulp.task('cssInject', ['styles'], function () {
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
+})
